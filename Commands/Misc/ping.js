e608bfc736b5638aa589f97a122fb658eaab9493
Jayke <69515083+Jayqe@users.noreply.github.com>
2021-04-12 22:23:56 +0100

    Update ping.js

--- Commands/Misc/ping.js
+++ Commands/Misc/ping.js
@@ -1,1 +1,22 @@
-//
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+        name: "ping",
+        description: "see your ping",
+        usage: "ping",
+        aliases: ['latency'],
+    run: async (bot, message, args) => {
+                const embed = new MessageEmbed()
+                .setTitle('Pong')
+                .setDescription('Test')
+                .addField(`Latency is :`, `${Date.now() - message.createdTimestamp}ms.`)
+                .addField(`API Latency is :`, `${Math.round(client.ws.ping)}ms`)
+                .setFooter('Made with <3 by Herdie#9541')
+                .setColor(0x00AE86)
+                message.reply(embed)
+        } catch (error) {
+                message.reply("**Something went wrong!**")
+                console.log(`Something went wrong! ${message.guild.id}`)
+              }
+};
+}
