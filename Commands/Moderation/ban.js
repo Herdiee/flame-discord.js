4d294cf6d4c3569d7b7035da2f70d723dbd77ac9
Jayke <69515083+Jayqe@users.noreply.github.com>
2021-04-12 22:30:00 +0100

    Update ban.js

--- Commands/Moderation/ban.js
+++ Commands/Moderation/ban.js
@@ -1,1 +1,81 @@
-pass
+// Dependancies
+const { MessageEmbed } = require('discord.js');
+const db = require('quick.db')
+const { ownerID } = require("../../owner.json")
+
+module.exports = {
+        name: "ban",
+        aliases: ["b", "banish"],
+        description: "Bans the user",
+        usage: "[name | nickname | mention | ID] <reason> (optional)",
+    run: async (bot, message, args) => {
+      
+        try {
+          
+            // If user does not have the correct permissions, return a permission error.
+            if (!message.member.hasPermission("BAN_MEMBERS") && !ownerID .includes(message.author.id)) return message.channel.send("**You Dont Have The Permissions To Ban Users! - [BAN_MEMBERS]**");
+            if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send("**I Dont Have The Permissions To Ban Users! - [BAN_MEMBERS]**");
+          
+            // If no arguments are provided, return arguments required error.
+            if (!args[0]) return message.channel.send("**Please Provide A User To Ban!**")
+
+            // Check the user is in the guild and is bannable
+            let banMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(ro => ro.displayName.toLowerCase() === args[0].toLocaleLowerCase());
+            if (!banMember) return message.channel.send("**User Is Not In The Guild**");
+            if (banMember === message.member) return message.channel.send("**You Cannot Ban Yourself**")
+            
+            // Get the punishment reason
+            var reason = args.slice(1).join(" ");
+
+            if (!banMember.bannable) return message.channel.send("**Cant Ban That User**")
+          
+            // DM The banned user including punishment reason.
+            try {
+            message.guild.members.ban(banMember)
+            banMember.send(`**Hello, You Have Been Banned From ${message.guild.name} for - ${reason || "No Reason"}**`).catch(() => null)
+            } catch {
+                message.guild.members.ban(banMember)
+            }
+            
+            // Include punishment reason in DMs
+            if (reason) {
+            var sembed = new MessageEmbed()
+                .setColor("GREEN")
+                .setDescription(`**${banMember.user.username}** has been banned for ${reason}`)
+            message.channel.send(sembed)
+            } else {
+                var sembed2 = new MessageEmbed()
+                .setColor("GREEN")
+                .setDescription(`**${banMember.user.username}** has been banned`)
+            message.channel.send(sembed2)
+            }
+
+            // Fetch logging channel from the database. if there isnt one, do nothing.
+            let channel = db.fetch(`modlog_${message.guild.id}`)
+            if (channel == null) return;
+
+            if (!channel) return;
+
+            // Logging embed
+            const embed = new MessageEmbed()
+                .setAuthor(`${message.guild.name} Moderation Logging`, message.guild.iconURL())
+                .setColor("#ff0000")
+                .setThumbnail(banMember.user.displayAvatarURL({ dynamic: true }))
+                .setFooter(message.guild.name, message.guild.iconURL())
+                .addField("**Moderation**", "ban")
+                .addField("**Banned**", banMember.user.username)
+                .addField("**ID**", `${banMember.id}`)
+                .addField("**Banned By**", message.author.username)
+                .addField("**Reason**", `${reason || "**No Reason**"}`)
+                .addField("**Date**", message.createdAt.toLocaleString())
+                .setTimestamp();
+
+            // Fetch the logging channel to send the logging embed.
+            var sChannel = message.guild.channels.cache.get(channel)
+            if (!sChannel) return;
+            sChannel.send(embed)
+        } catch (e) {
+            return message.channel.send(`**${e.message}**`)
+        }
+    }
+};
