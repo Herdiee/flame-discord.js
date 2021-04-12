f060bf0a4df5ebc70f55842a5ac1ee5ccfd3c212
Jayke <69515083+Jayqe@users.noreply.github.com>
2021-04-12 21:58:27 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,1 +1,13 @@
-
+module.exports = (client) => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Flame Discord client By Edgewurth`);
+    console.log(`serving ${client.guilds.cache.size} servers`);
+    client.user.setPresence({
+        status: 'available',
+        activity: {
+            name: 'Playing with flames',
+            type: 'STREAMING',
+            url: 'https://www.youtube.com/watch?v=iik25wqIuFo'
+        }
+    })
+};
