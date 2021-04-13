fa77bac1b91e470df8b140303bcc7642c77685e5
Jayke <69515083+Jayqe@users.noreply.github.com>
2021-04-13 07:06:24 +0100

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -23,29 +23,6 @@ module.exports = (client, message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-  
-  
-  // Cooldown
-  const { cooldowns } = client;
-  if (!cooldowns.has(command.name)) {
-    cooldowns.set(command.name, new Discord.Collection());
-  }
-  const now = Date.now();
-  const timestamps = cooldowns.get(command.name);
-  
-  // Default Cooldown time is 3 seconds, although this can vary depending on the command.
-  const cooldownAmount = (command.cooldown || 3) * 1000;
-  
-  if (timestamps.has(message.author.id)) {
-    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-    if (now < expirationTime) {
-      const timeLeft = (expirationTime - now) / 1000;
-      return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command}\` command.`);
-    }
-  }
-  timestamps.set(message.author.id, now);
-  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
   // Grab the command data from the client.commands Enmap
   const cmd = client.commands.get(command)
     || client.commands.find(commandName => commandName.aliases && commandName.aliases.includes(command));
