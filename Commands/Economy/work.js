d66229ed65af21580a5ae4332d388e5a9f98301d
Jayke <69515083+Jayqe@users.noreply.github.com>
2021-05-03 14:27:22 +0100

    Create work.js

---
+++ Commands/Economy/work.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const db = require("quick.db");
+
+module.exports = {
+      name: 'work',
+      description: 'Earn money by working',
+      cooldown: 300,
+      aliases: ['wk'],
+  
+  run: async (client, message, args) => {
+    let author = message.author;
+    let author = await db.fetch(`money_${message.guild.id}_${user.id}`)
+
+    let jobs = [
+    'a Fast Food Restaurant',
+    'an Supermarket',
+    'a School',
+    'a Swimming Pool',
+    'an Department Store',
+    'an Hotel',
+    'the Government',
+    'the Police',
+    'the Fire Department',
+    'the Hospital',
+    'an Insurance Company',
+    'a Football Club',
+    'an Film Production Team',
+    'an Funeral Director\'s Company',
+    'an IT Repair Shop',
+    'an Video Game Development Company',
+    'a Museum.',
+    ];
+
+  let jobworked = Math.floor((Math.random() * jobs.length));
+  let amount = 1
+
+  let embed = new MessageEmbed()
+  .setColor("#FFFFFF")
+  .setDescription(`You worked as a ${jobs[jobworked]{ and earned ${amount}`)
+  message.channel.send(embed) 
+  await client.db.add(`money_${message.guild.id}_${user.id}.pocket`, amount)
+    
+    
+    
+    
