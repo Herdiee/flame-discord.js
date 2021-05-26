006ea5173c672576a41f0da19193e9dd24c16d32
Jayke <61476603+Herdiee@users.noreply.github.com>
2021-05-26 17:46:36 +0100

    Relocated Workplaces

--- Commands/Economy/work.js
+++ Commands/Economy/work.js
@@ -9,40 +9,26 @@ module.exports = {
     aliases: ['wk'],
   
   run: async (client, message, args) => {   
-    let authorid = message.author.id
+    try {
+      let authorid = message.author.id
 
-    authorbal = await db.fetch(`money_${message.guild.id}_${authorid}`)
-    if (authorbal === null) authorbal = 0; 
+      authorbal = await db.fetch(`money_${message.guild.id}_${authorid}`)
+      if (authorbal === null) authorbal = 0; 
 
-    let jobs = [                                                           
-    'a Fast Food Restaurant',
-    'an Supermarket',
-    'a School',
-    'a Swimming Pool',
-    'an Department Store',
-    'an Hotel',
-    'the Government',
-    'the Police',
-    'the Fire Department',
-    'the Hospital',
-    'an Insurance Company',
-    'a Football Club',
-    'an Film Production Team',
-    'an Funeral Director\'s Company',
-    'an IT Repair Shop',
-    'an Video Game Development Company',
-    'a Museum.',
-    ];
+      let jobs = require('../../Resources/workplaces.js')
 
-    let jobworked = Math.floor(Math.random() * jobs.length);                        // Pick a random job
-    let amount = Math.floor(Math.random() * 100);                                   // Pick a random amount
+      let jobworked = Math.floor(Math.random() * jobs.length);                        // Pick a random job
+      let amount = Math.floor(Math.random() * 100);                                   // Pick a random amount
 
-    let embed = new MessageEmbed()                                                  // Embed Creation
-    .setColor("#FFFFFF")
-    .setDescription(`You worked as a ${jobs[jobworked]} and earned ${amount}`)
-    message.reply(embed) 
+      let embed = new MessageEmbed()                                                  // Embed Creation
+      .setColor("#FFFFFF")
+      .setDescription(`You worked at ${jobs[jobworked]} and earned ${amount}`)
+      .setColor(0x00AE86)
+      message.reply(embed) 
 
-    let a = await db.add(`money_${message.guild.id}_${authorid}`, amount)            // Add money to the database
-    
+      let a = await db.add(`money_${message.guild.id}_${authorid}`, amount)            // Add money to the database
+  } catch (error) {
+      console.log(error.message)
+  }
 }
 }
---
+++ Resources/workplaces.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js"); 
+const { MessageEmbed } = require('discord.js');
+
+let jobs = [                                                           
+    'a Fast Food Restaurant',
+    'an Supermarket',
+    'a School',
+    'a Swimming Pool',
+    'an Department Store',
+    'an Hotel',
+    'the Government',
+    'the Police',
+    'the Fire Department',
+    'the Hospital',
+    'an Insurance Company',
+    'a Football Club',
+    'an Film Production Team',
+    'an Funeral Director\'s Company',
+    'an IT Repair Shop',
+    'an Video Game Development Company',
+    'a Museum.',
+    ];
+
+module.exports = (jobs);
